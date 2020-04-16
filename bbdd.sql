d76691e609abbb74c25f4bbbd976cf3fc6998102
fedefru <fru.federico@gmail.com>
2020-04-16 12:40:18 -0300

    Commit proyect

---
+++ bbdd.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE IF NOT EXISTS curso_angular4;
+USE curso_angular4;
+
+CREATE TABLE productos (
+    id             int(255) auto_increment not NULL,
+    nombre         varchar(255),
+    descripcion    text,
+    precio         varchar(255),
+    imagen         varchar(255),
+    CONSTRAINT pk_productos PRIMARY KEY(id)
+) ENGINE = InnoDb;
